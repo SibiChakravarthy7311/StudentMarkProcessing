1086925130b596009af30dc42ba22a97db8c4011
dhineshkumar <dhineshkumar315@gmail.com>
2021-04-03 12:51:14 +0530

    Update

--- app/src/main/java/com/example/studentmarkprocessing/studentMarksActivity.java
+++ app/src/main/java/com/example/studentmarkprocessing/studentMarksActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.studentmarkprocessing.databinding.ActivityStudentBinding;
@@ -19,8 +20,12 @@ import com.google.firebase.database.ValueEventListener;
 public class studentMarksActivity extends AppCompatActivity {
 
     private ActivityStudentMarksBinding binding;
-    private FirebaseDatabase student;
-    private DatabaseReference ref1;
+
+    private FirebaseDatabase db;
+    private DatabaseReference ref;
+    private EditText rollno,cgpa,name;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,66 +33,35 @@ public class studentMarksActivity extends AppCompatActivity {
         binding = ActivityStudentMarksBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        //setContentView(R.layout.activity_student_marks);
 
-        /*
-        String name1;
+        db=FirebaseDatabase.getInstance();
+        ref=db.getReference("StudentDetails");
 
-        student = FirebaseDatabase.getInstance();
-        ref = student.getReference();
-        /*
+        rollno=(EditText)findViewById(R.id.roll_no);
+        cgpa=(EditText)findViewById(R.id.cgpa);
+        name=(EditText)findViewById(R.id.name);
+
+        rollno.setText("18BCS013");
+
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                //Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
-                //String name1 = snapshot.child("18BCS001").child("Name").getValue(String.class);
-                //binding.name.setText(name1);
-                Post post = snapshot.getValue();
+                String Name=snapshot.child(rollno.getText().toString()).child("Name").getValue(String.class);
+                String CGPA=snapshot.child(rollno.getText().toString()).child("CGPA").getValue(String.class);
+                cgpa.setText(CGPA);
+                name.setText(Name);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(getApplicationContext(), "Failure", Toast.LENGTH_SHORT).show();
+
             }
         });
-
-
-        Query query = ref.child("StudentDetails");
-        //Query query = ref.child("Name");
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Toast.makeText(getApplicationContext(), "Powerrr!!!!", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(getApplicationContext(), "Directed by Robert.B.Weide", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-         */
 
         binding.btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ref1 = FirebaseDatabase.getInstance().getReference().child("StudentDetails").child("18BCS001");
-                ref1.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        String name1 =snapshot.child("Name").getValue().toString();
-                        String cgpa1 =snapshot.child("CGPA").getValue().toString();
-                        String sem =snapshot.child("Semester").getValue().toString();
-                        binding.name.setText(name1);
-                        binding.semester.setText(sem);
-                        binding.cgpa.setText(cgpa1);
-                    }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
             }
         });
     }
